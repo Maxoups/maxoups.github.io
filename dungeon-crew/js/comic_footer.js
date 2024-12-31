cf28bb57fd7bca6fd6b09e1088fecc85cee6f1b6
Maxou <mm.maxencemaire@gmail.com>
2024-12-31 15:39:17 +0100

    zoom not working; try with min scale = 0.0

--- dungeon-crew/js/comic_footer.js
+++ dungeon-crew/js/comic_footer.js
@@ -2,7 +2,7 @@
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
 document.querySelector(".writeFooter").innerHTML = `
     <footer align="center">
-        <p>V1.0.0</p>
+        <p>V1.0.1</p>
         <p>Thanks for reading :)</p>
         <p>Here's a link to Rarebit. It's what I used to make this part of the website and it's really cool and I like it.</p> 
         <p><strong>Powered by</strong></p>
