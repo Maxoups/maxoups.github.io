a1c7dae407b772438bd4eb1014a0752b7fcecdae
Maxou <mm.maxencemaire@gmail.com>
2025-01-02 23:15:24 +0100

    added master description

--- js/writeProjectBubbles.js
+++ js/writeProjectBubbles.js
@@ -11,20 +11,20 @@ class BubbleData {
 }
 
 let projects = {
-    "lab": new BubbleData(
+    "ooc": new BubbleData(
         "Organ-on-a-chip Research", "url('images/projects/okayama_research.jpg')", 
         "As part of my Master's degree, I worked on a bioengineering research project at the University of Okayama in Japan, under the guidance of Professor K. Takahashi.", 
         "https://ken-takahashi.net/french-sorbonne-university-student-executes-fluid-dynamics-simulation-project-for-organ-chips/", 
         "10%", "5%"),
-    "lab2": new BubbleData(
-        "Project 1", "url('images/projects/okayama_research.jpg')", 
-        "This is a placeholder for project 1", 
-        "project1.html", 
-        "70%", "115%"),
+    "master": new BubbleData(
+        "Master in Computer Science", "url('images/projects/remise_masters.jpg')", 
+        "In 2024, I graduated from Sorbonne University with a Master's degree in Human-Computer Interaction, Multi-Agent Systems, Artificial Intelligence and Robotics.", 
+        "https://sciences.sorbonne-universite.fr/formation-sciences/masters/master-informatique/parcours-ai2d", 
+        "40%", "115%"),
     "project2": new BubbleData("Project 2", 
         "url('images/projects/okayama_research.jpg')", 
         "This is a placeholder for project 2", 
-        "project2.html", 
+        "https://maxoups.github.io/", 
         "5%", "0%"),
 }
 
@@ -34,9 +34,9 @@ writeBubbles();
 function writeBubbles() {
     let writeNavDiv = document.querySelectorAll(".projectBubbles");
     writeNavDiv.forEach(function(element) {
-      element.innerHTML = `<div class="projectBubblesWrapper" style="min-height: 900px;min-width: 900px; ">
-        ${projectBubble(projects["lab"], true, true)}
-        ${projectBubble(projects["lab2"], true, false)}
+      element.innerHTML = `<div class="projectBubblesWrapper" style="min-height: 900px;min-width: 100%; ">
+        ${projectBubble(projects["ooc"], true, true)}
+        ${projectBubble(projects["master"], true, false)}
         ${projectBubble(projects["project2"], false, false)}
         </div>
         `;})
@@ -59,10 +59,10 @@ function writeBubbles() {
             bottom_dot = `<span class="dot" style="position:absolute; bottom:-20px; left:`+ bottom_dot_posX +`%"></span>`;
         }
         return `
-        <a href="`+ bubbleData.link +`" target="_blank" style="text-decoration: none;">
+        <a href="`+ bubbleData.link +`" target="_blank" style="text-decoration:none; max-height: 200px; max-width: 300px;">
             <div class="workBubble" 
                 style="background-image:`+ bubbleData.image +`;
-                top: `+ bubbleData.posY +`; left: `+ bubbleData.posX +`;
+                top: `+ bubbleData.posY +`; margin-left: `+ bubbleData.posX +`;
                 animation-delay: `+ anim_delay +`s;">
                 `+ top_dot +`
                 `+ bottom_dot +`
