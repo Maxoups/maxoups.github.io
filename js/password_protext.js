9083c8dd5c4d063d1bbb62832cfc40d81b1f6c07
Maxou <mm.maxencemaire@gmail.com>
2025-01-07 17:29:01 +0100

    Update password_protext.js
    .

--- js/password_protext.js
+++ js/password_protext.js
@@ -1,6 +1,4 @@
 
-
-
 async function hashSHA256(input) {
     const encoder = new TextEncoder();
     const data = encoder.encode(input);
@@ -25,43 +23,3 @@ document.getElementById('hashForm').addEventListener('submit', async (event) => 
         alert('Wrong password!');
     }
 });
-
-
-
-
-`
-
-
-function password_protection() {
-    // key = 7308
-    var o = document.getElementById('protect-overlay');
-    o.getElementsByTagName('form')[0].onsubmit = (async () => {
-        const hashedValue = await hashSHA256(this.answer.value);
-        console.log("Hashed Value:", hashedValue);
-        if (hashedValue == 'abba884411e3b916a2e4e7f5bfae98964f9b3fc98d16e5ce28ba8d5103e29de0') {
-            console.log("Correct Password!");
-            window.location.replace("arg_"+ this.answer.value +".html");
-        } else {
-            console.log("Wrong Password!");
-            alert('Wrong password!');
-        }
-        return false;
-    })();
-  }
-
-
-
-function password_protectionOLD() {
-    var o = document.getElementById('protect-overlay');
-    o.getElementsByTagName('form')[0].onsubmit = function() {
-      if (hashSHA256(this.answer.value) == 'abba884411e3b916a2e4e7f5bfae98964f9b3fc98d16e5ce28ba8d5103e29de0') {
-        console.log(this.answer.value + ": correct password!");
-        window.location.replace("arg_"+ this.answer.value +".html");
-      } else {
-        console.log(this.answer.value + ": wrong password!");
-        alert('Wrong password!');
-      }
-      return false;
-    };
-  }`
-
