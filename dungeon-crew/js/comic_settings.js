07be44ce0a0372a93b2bcf647154931a14769f61
Maxou <mm.maxencemaire@gmail.com>
2025-01-19 22:12:39 +0100

    added #8 preview

--- dungeon-crew/js/comic_settings.js
+++ dungeon-crew/js/comic_settings.js
@@ -11,7 +11,7 @@ let pg = Number(findGetParameter("pg")); //make "pg" mean the current page numbe
 ////////////////////////
 
 //REALLY IMPORTANT ONES
-const maxpg = 7; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
+const maxpg = 8; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
 //YOU MUST UPDATE THIS NUMBER EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
@@ -123,6 +123,14 @@ const pgData = [
         pgNum: 7,
         title: "A short study of dungeons... 1/2",
         date: writeDate(2025, 1, 19),
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 8,
+        title: "[PREVIEW]",
+        date: writeDate(2025, 1, 26),
         altText: "",
         imageFiles: 1,
         authorNotes: ``,
