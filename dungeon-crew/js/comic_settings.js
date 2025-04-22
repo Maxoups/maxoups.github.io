294c865d6069b9ed23fe30a5a57959c2428194d4
Maxou <mm.maxencemaire@gmail.com>
2025-04-22 19:39:34 +0200

    Update comic_settings.js

--- dungeon-crew/js/comic_settings.js
+++ dungeon-crew/js/comic_settings.js
@@ -11,7 +11,7 @@ let pg = Number(findGetParameter("pg")); //make "pg" mean the current page numbe
 ////////////////////////
 
 //REALLY IMPORTANT ONES
-const maxpg = 17 +1; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
+const maxpg = 18 +1; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
 //YOU MUST UPDATE THIS NUMBER EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
@@ -208,9 +208,17 @@ const pgData = [
         authorNotes: ``,
     },
     {
+        pgNum: 18,
+        title: "King's Quest 2/2",
+        date: writeDate(2025, 4, 27),
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
+    },
+    {
         pgNum: maxpg,
         title: "[WORK IN PROGRESS]",
-        date: writeDate(2025, 4, 27),
+        date: writeDate(2025, 5, 4),
         altText: "Coming soon!",
         imageFiles: 1,
         authorNotes: ``,
