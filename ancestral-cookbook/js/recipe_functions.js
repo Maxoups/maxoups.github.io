6f451242698443aed0a1dd3370a0c7d514881939
Maxoups <mm.maxencemaire@gmail.com>
2025-08-25 15:51:54 +0200

    Update recipe_functions.js

--- ancestral-cookbook/js/recipe_functions.js
+++ ancestral-cookbook/js/recipe_functions.js
@@ -42,30 +42,24 @@ Papa.parse(CsvFile, {
     header: true,     // first row is header
     skipEmptyLines: true,
     complete: function(results) {
-        console.log("CSV Parsed:")
-        
         // Assume we take the first row (first recipe)
         const data = results.data[0];
 
-        console.log("results.data[0]:")
-        console.log(data)
-
         // Map CSV fields to variables
-        const recipeTitle = data.title;
-        const recipeCook = data.cook;
-        const recipeType = data.type;
-        const recipeTags = data.tags ? data.tags.split(";") : [];
-        const recipeComments = data.comments;
-        const recipeTime = data.time;
-        const recipeIngredients = data.ingredients ? data.ingredients.split(";") : [];
-        const recipeText = data.text ? data.text.split(";") : [];
-        const recipeImages = data.images ? data.images.split(";") : [];
+        const recipeTitle = data.Titre;
+        const recipeCook = data.Cuistot;
+        const recipeType = data.Type;
+        const recipeTags = data.Tags ? data.Tags.split(", ") : [];
+        const recipeComments = data.Commentaires;
+        const recipeTime = data.Temps;
+        const recipeIngredients = data.Ingrédients ? data.Ingrédients.split("\n") : [];
+        const recipeText = data.Recette ? data.Recette.split("\n\n") : [];
+        const recipeImages = data.Images ? data.Images.split(", ") : [];
 
         // Create HTML div for this recipe
-
         createRecipeSlider(recipeImages);
         addRecipeTitle(recipeTitle);
-        addRecipeChef(recipeCook)
+        addRecipeChef(recipeCook);
     },
     error: function(err) {
         console.error("Error parsing CSV:", err);
