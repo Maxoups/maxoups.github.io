ef932e07b6568072e78ff4221b043e14c94ba677
Maxoups <mm.maxencemaire@gmail.com>
2025-08-25 16:03:46 +0200

    Update recipe_functions.js

--- ancestral-cookbook/js/recipe_functions.js
+++ ancestral-cookbook/js/recipe_functions.js
@@ -22,60 +22,62 @@ function addRecipeChef(text) {
     document.getElementById("recipe-chef").innerText = text
 }
 
-function parseCSV(csvString) {
-    return Papa.parse(csvString, {
-        download: true,
-        header: true, // first row as header
-        skipEmptyLines: true
-    });
+
+//#####################################################################//
+
+
+function findRecipeByTitle(recipeTitle) {
+    const data = allRecipes.find(row => row.Titre === pageTitle);
+    if (!data) {
+        console.log("Recipe not found!")
+        return;
+    }
+    return data;
+}
+
+function buildPage(data) {
+    // Map CSV fields to variables
+    const recipeTitle = data.Titre;
+    const recipeCook = data.Cuistot;
+    const recipeType = data.Type;
+    const recipeTags = data.Tags ? data.Tags.split(", ") : [];
+    const recipeComments = data.Commentaires;
+    const recipeTime = data.Temps;
+    const recipeIngredients = data.Ingrédients ? data.Ingrédients.split("\n") : [];
+    const recipeText = data.Recette ? data.Recette.split("\n\n") : [];
+    const recipeImages = data.Images ? data.Images.split(", ") : [];
+
+    // Create HTML div for this recipe
+    createRecipeSlider(recipeImages);
+    addRecipeTitle(recipeTitle);
+    addRecipeChef(recipeCook);
 }
 
 
 //#####################################################################//
 
-console.log("Hello world!")
-const CsvFile = "https://maxoups.github.io/ancestral-cookbook/Recettes.csv"
+let recipesData = [];
 
-// Parse CSV and fill page
-Papa.parse(CsvFile, {
-    download: true,   // load the file via HTTP
-    header: true,     // first row is header
-    skipEmptyLines: true,
-    complete: function(results) {
-        // Assume we take the first row (first recipe)
-        const data = results.data[0];
+function parseRecipesCSV() {
+    const CsvFile = "https://maxoups.github.io/ancestral-cookbook/Recettes.csv"
 
-        // Map CSV fields to variables
-        const recipeTitle = data.Titre;
-        const recipeCook = data.Cuistot;
-        const recipeType = data.Type;
-        const recipeTags = data.Tags ? data.Tags.split(", ") : [];
-        const recipeComments = data.Commentaires;
-        const recipeTime = data.Temps;
-        const recipeIngredients = data.Ingrédients ? data.Ingrédients.split("\n") : [];
-        const recipeText = data.Recette ? data.Recette.split("\n\n") : [];
-        const recipeImages = data.Images ? data.Images.split(", ") : [];
+    // Parse CSV and fill page
+    Papa.parse(CsvFile, {
+        download: true,   // load the file via HTTP
+        header: true,     // first row is header
+        skipEmptyLines: true,
+        complete: function(results) {
+            // Assume we take the first row (first recipe)
+            recipesData = results.data
 
-        // Create HTML div for this recipe
-        createRecipeSlider(recipeImages);
-        addRecipeTitle(recipeTitle);
-        addRecipeChef(recipeCook);
-    },
-    error: function(err) {
-        console.error("Error parsing CSV:", err);
-    }
-});
+            
+        },
+        error: function(err) {
+            console.error("Error parsing CSV:", err);
+        }
+    });
+}
 
+parseRecipesCSV("Tiramisu")
 
-/*
-// Parse CSV
-let parsedRecipes = Papa.parse("Recettes.csv", {
-    download: true,
-    header: true, // first row as header
-    skipEmptyLines: true
-});
-console.log("parsedRecipes:")
-console.log(parsedRecipes)
-*/
-// Function calls
 
