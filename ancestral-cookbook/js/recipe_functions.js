ee4f323fe72f095cbfbec54a1886ebb55fd03b34
Maxoups <mm.maxencemaire@gmail.com>
2025-08-25 15:19:08 +0200

    Update recipe_functions.js

--- ancestral-cookbook/js/recipe_functions.js
+++ ancestral-cookbook/js/recipe_functions.js
@@ -32,28 +32,56 @@ function parseCSV(csvString) {
 
 
 //#####################################################################//
-// Recipe variables
-const recipeTitle = "Vegetarian butter salad"
-const recipeCook  = "Stéphane Maire"
-const recipeType = []
-const imageList   = ["img/bg-img/bg5.jpg", "img/tiramisu02.jpg"];
+
+console.log("Hello world!")
+const CsvFile = "https://maxoups.github.io/ancestral-cookbook/Recettes.csv"
+
+// Parse CSV and fill page
+Papa.parse(CsvFile, {
+    download: true,   // load the file via HTTP
+    header: true,     // first row is header
+    skipEmptyLines: true,
+    complete: function(results) {
+        console.log("CSV Parsed:")
+        
+        // Assume we take the first row (first recipe)
+        const data = results.data[0];
+
+        console.log("results.data[0]:")
+        console.log(data)
+
+        // Map CSV fields to variables
+        const recipeTitle = data.title;
+        const recipeCook = data.cook;
+        const recipeType = data.type;
+        const recipeTags = data.tags ? data.tags.split(";") : [];
+        const recipeComments = data.comments;
+        const recipeTime = data.time;
+        const recipeIngredients = data.ingredients ? data.ingredients.split(";") : [];
+        const recipeText = data.text ? data.text.split(";") : [];
+        const recipeImages = data.images ? data.images.split(";") : [];
+
+        // Create HTML div for this recipe
+
+        createRecipeSlider(recipeImages);
+        addRecipeTitle(recipeTitle);
+        addRecipeChef(recipeCook)
+    },
+    error: function(err) {
+        console.error("Error parsing CSV:", err);
+    }
+});
 
 
-
+/*
 // Parse CSV
 let parsedRecipes = Papa.parse("Recettes.csv", {
     download: true,
     header: true, // first row as header
     skipEmptyLines: true
 });
-console.log("parsedRecipes.data:")
-console.log(parsedRecipes.data)
-console.log("parsedRecipes.errors:")
-console.log(parsedRecipes.errors)
-console.log("parsedRecipes.meta:")
-console.log(parsedRecipes.meta)
-
+console.log("parsedRecipes:")
+console.log(parsedRecipes)
+*/
 // Function calls
-createRecipeSlider(imageList);
-addRecipeTitle(recipeTitle);
-addRecipeChef(recipeCook)
+
