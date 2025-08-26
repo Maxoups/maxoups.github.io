15499121b54bd4532b2ba10593f81e08d5378c57
Maxoups <mm.maxencemaire@gmail.com>
2025-08-26 12:48:38 +0200

    put header in js function

--- ancestral-cookbook/js/recipe_functions.js
+++ ancestral-cookbook/js/recipe_functions.js
@@ -76,6 +76,101 @@ function addRecipeSteps(steps) {
     stepsList.innerHTML = stepsHTML;
 }
 
+function writeHeader() {
+    document.getElementById("website-header").innerHTML = `<!-- Preloader -->
+    <div id="preloader">
+        <i class="circle-preloader"></i>
+        <img src="img/core-img/salad.png" alt="">
+    </div>
+
+    <!-- Search Wrapper -->
+    <div class="search-wrapper">
+        <!-- Close Btn -->
+        <div class="close-btn"><i class="fa fa-times" aria-hidden="true"></i></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <form action="#" method="post">
+                        <input type="search" name="search" placeholder="Type any keywords...">
+                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+        <!-- Navbar Area -->
+        <div class="delicious-main-menu" style="max-height: 100px;">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="deliciousNav">
+
+                        <!-- Logo -->
+                        <a class="nav-brand" href="index.html"><img class="img-logo" src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="active"><a class="list-link" href="index.html">Accueil</a></li>
+                                    <li><a class="list-link" href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a class="list-item" href="index.html">Générer un menu</a></li>
+                                            <li><a class="list-item" href="blog-post.html">Rechercher</a></li>
+                                            <li><a class="list-item" href="about.html">À propos</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a class="list-link" href="#">Menu</a>
+                                        <ul class="dropdown">
+                                            <li><a class="list-item" href="index.html">Entrées</a></li>
+                                            <li><a class="list-item" href="blog-post.html">Plats</a></li>
+                                            <li><a class="list-item" href="about.html">Desserts</a></li>
+                                            <li><a class="list-item" href="about.html">Boissons</a></li>
+                                            <li><a class="list-item" href="about.html">Autres</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a class="list-link" href="#">Cuistots</a>
+                                        <ul class="dropdown">
+                                            <li><a class="list-item" href="recipe-post.html">Kaki</a></li>
+                                            <li><a class="list-item" href="recipe-post.html">Mélinouche</a></li>
+                                        </ul>
+                                    </li>
+                                    <!-- <li><a href="contact.html">Contact</a></li> -->
+                                </ul>
+
+                                <!-- Newsletter Form -->
+                                <div class="search-btn">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </div>
+
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->`;
+}
+
 
 
 //#####################################################################//
@@ -116,9 +211,10 @@ function buildPage(data) {
 }
 
 function writePage() {
+    writeHeader();
     if (pageName) {
-        const data = findRecipeByTitle(pageName)
-        buildPage(data)
+        const data = findRecipeByTitle(pageName);
+        buildPage(data);
     }
 }
 
